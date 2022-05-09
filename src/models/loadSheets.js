d55bb8ded84c139b9cc7592c8f477d1a8e66d608
Diego R. Lorenesi <drlorenesi@gmail.com>
2022-05-08 19:42:48 -0600

    Agregar .replace() a GOOGLE_PRIVATE_KEY.

--- src/models/loadSheets.js
+++ src/models/loadSheets.js
@@ -8,7 +8,7 @@ exports.loadUsers = async function () {
   // Service account details
   await doc.useServiceAccountAuth({
     client_email: process.env.GOOGLE_SERVICE_ACCOUNT_EMAIL,
-    private_key: process.env.GOOGLE_PRIVATE_KEY,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   });
   // Load the document info
   await doc.loadInfo();
