9cb875b37fe93480387c0673c40397ff37dd1d30
Diego R. Lorenesi <drlorenesi@gmail.com>
2022-05-08 19:31:50 -0600

    Agregar ruta de estado de API.

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,5 +1,7 @@
 const usuarios = require('../routes/usuarios');
+const estado = require('../routes/estado');
 
 module.exports = (app) => {
   app.use('/v1/usuarios', usuarios);
+  app.use('/v1/estado', estado);
 };
